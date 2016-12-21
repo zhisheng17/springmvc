48a835dead422f93462dbed7143262a5e6a44216
zhisheng17 <1041218129@qq.com>
2016-12-21 21:00:38 +0800

    Spring MVC集成Hibernate JPA的博客项目测试成功

---
+++ src/main/java/cn/zhisheng/model/BlogEntity.java
@@ -0,0 +1,92 @@
+package cn.zhisheng.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by 10412 on 2016/12/21.
+ */
+@Entity
+@Table(name = "blog", schema = "springdemo", catalog = "")
+public class BlogEntity
+{
+    private int id;
+    private String title;
+    private String content;
+    private Date pubDate;
+    private UserEntity userByUserId;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title", nullable = false, length = 45)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(name = "content", nullable = true, length = 45)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Basic
+    @Column(name = "pub_date", nullable = false)
+    public Date getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(Date pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogEntity that = (BlogEntity) o;
+
+        if (id != that.id) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (content != null ? !content.equals(that.content) : that.content != null) return false;
+        if (pubDate != null ? !pubDate.equals(that.pubDate) : that.pubDate != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (pubDate != null ? pubDate.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    public UserEntity getUserByUserId() {
+        return userByUserId;
+    }
+
+    public void setUserByUserId(UserEntity userByUserId) {
+        this.userByUserId = userByUserId;
+    }
+}
