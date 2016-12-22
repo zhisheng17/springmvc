522e3551a271f77779bfa75cb316f937b1094102
zhisheng17 <1041218129@qq.com>
2016-12-22 17:11:53 +0800

    完成Spring MVC博客项目用户管理

--- src/main/java/cn/zhisheng/controller/IndexController.java
+++ src/main/java/cn/zhisheng/controller/IndexController.java
@@ -5,6 +5,8 @@ import cn.zhisheng.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,6 +31,7 @@ public class IndexController
         return "index";
     }
 
+    //用户列表
     @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
     public String getUser(ModelMap modelMap)
     {
@@ -43,6 +46,85 @@ public class IndexController
     }
 
 
+    //访问添加用户界面
+    @RequestMapping(value = "/admin/users/add", method = RequestMethod.GET)
+    public String addUser()
+    {
+        return "admin/addUser";
+    }
+
+
+    //处理添加用户请求，并重定向到用户管理界面
+    @RequestMapping(value = "/admin/users/addP", method = RequestMethod.POST)
+    public String addUserPost(@ModelAttribute("user") UserEntity userEntity)
+    {
+        // 注意此处，post请求传递过来的是一个UserEntity对象，里面包含了该用户的信息
+        // 通过@ModelAttribute()注解可以获取传递过来的'user'，并创建这个对象
+
+        // 数据库中添加一个用户，该步暂时不会刷新缓存,save()方法处理完毕后，数据依然在缓冲区未写入数据库
+        //userRepository.save(userEntity);
+
+        // 数据库中添加一个用户，并立即刷新缓存并写入数据库
+        userRepository.saveAndFlush(userEntity);
+
+        // 重定向到用户管理页面，方法为 redirect:url
+        return "redirect:/admin/users";
+    }
+
+
+    //查看用户信息详情
+    //@PathVariable可以收集url中的变量，需匹配的变量用{}括起来
+    // 例如：访问 localhost:8080/admin/users/show/1 ，将匹配 id = 1
+    @RequestMapping(value = "/admin/users/show/{id}", method = RequestMethod.GET)
+    public String showUser(@PathVariable("id") Integer userId, ModelMap modelMap)
+    {
+        //找到userId所表示的用户
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        //传递给请求页面
+        modelMap.addAttribute("user", userEntity);
+
+        //转到用户详情页面
+        return "admin/userDetail";
+    }
+
+
+    //更新用户信息 页面
+    @RequestMapping(value = "/admin/users/update/{id}", method = RequestMethod.GET)
+    public String updateUser(@PathVariable("id") Integer userId, ModelMap modelMap)
+    {
+        //找到userId所表示的用户
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        //传递给请求页面
+        modelMap.addAttribute("user", userEntity);
+
+        return "admin/updateUser";
+    }
+
+    //更新用户信息 操作
+    @RequestMapping(value = "/admin/users/updateP", method = RequestMethod.POST)
+    public String updateUserPost(@ModelAttribute("userP") UserEntity user)
+    {
+        //更新用户信息
+        userRepository.updateUser(user.getNickname(), user.getFirstName(), user.getLastName(), user.getPassword(), user.getId());
+
+        //刷新缓冲区
+        userRepository.flush();
+        return "redirect:/admin/users";
+    }
+
+    //删除用户
+    @RequestMapping(value = "/admin/users/delete/{id}", method = RequestMethod.GET)
+    public String deleteUser(@PathVariable("id") Integer userId)
+    {
+        //指定删除id为userId的用户
+        userRepository.delete(userId);
+
+        //刷新缓存区
+        userRepository.flush();
+        return "redirect:/admin/users";
+    }
 
 
 }
--- src/main/java/cn/zhisheng/repository/UserRepository.java
+++ src/main/java/cn/zhisheng/repository/UserRepository.java
@@ -2,7 +2,11 @@ package cn.zhisheng.repository;
 
 import cn.zhisheng.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by 10412 on 2016/12/21.
@@ -10,5 +14,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer>
 {
+    @Modifying          //说明该方法是修改方法
+    @Transactional      //说明该方法是事性操作
+
+    // 定义查询
+    // @Param注解用于提取参数
+    @Query("update UserEntity us set us.nickname=:qNickname, us.firstName=:qFirstName, us.lastName=:qLastName, us.password=:qPassword where us.id=:qId")
+    public void updateUser(@Param("qNickname") String nickname, @Param("qFirstName") String firstName,
+                           @Param("qLastName") String lastName, @Param("qPassword") String password, @Param("qId") Integer id);
+
+
+
 
 }
